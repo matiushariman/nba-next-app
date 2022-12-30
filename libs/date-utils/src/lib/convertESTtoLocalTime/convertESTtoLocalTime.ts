8f1003b5bc16d4b287188cce52223e5201534be8
matiushariman <matius.hariman@gmail.com>
2022-12-30 11:38:55 +0700

    [date-utils] update tsconfig esModuleInterop

--- libs/date-utils/src/lib/convertESTtoLocalTime/convertESTtoLocalTime.ts
+++ libs/date-utils/src/lib/convertESTtoLocalTime/convertESTtoLocalTime.ts
@@ -1,6 +1,6 @@
-import * as dayjs from 'dayjs';
-import * as utc from 'dayjs/plugin/utc';
-import * as timezone from 'dayjs/plugin/timezone';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
 export const convertESTtoLocalTime = (ESTTime: Date | string | dayjs.Dayjs) => {
   dayjs.extend(utc);
--- libs/date-utils/src/lib/formatDate/formatDate.ts
+++ libs/date-utils/src/lib/formatDate/formatDate.ts
@@ -1,4 +1,4 @@
-import * as dayjs from 'dayjs';
+import dayjs from 'dayjs';
 
 export const formatDate = (
   date: dayjs.Dayjs | Date | string | number | null,
