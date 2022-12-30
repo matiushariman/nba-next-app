d2dceb8d57fe44290c7ed1fe9ed8aea999c99f49
matiushariman <matius.hariman@gmail.com>
2022-12-30 13:34:45 +0700

    [nba-client] add test cases for TodayGame

--- libs/test-utils/src/lib/TestUtils.tsx
+++ libs/test-utils/src/lib/TestUtils.tsx
@@ -7,44 +7,46 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { SWRConfig } from 'swr';
 
-const AllTheProviders: FC<{ children: ReactNode }> = ({ children }) => {
-  const [mode, setMode] = useState<PaletteMode>('light');
-
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    []
-  );
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode]
-  );
-
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <SWRConfig value={{ dedupingInterval: 0 }}>{children}</SWRConfig>
-        </LocalizationProvider>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
-};
-
 const customRender = (
   ui: ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>
-) => render(ui, { wrapper: AllTheProviders, ...options });
+  options?: Omit<RenderOptions, 'wrapper'> & { mode?: PaletteMode }
+) => {
+  const AllTheProviders: FC<{ children: ReactNode }> = ({ children }) => {
+    const [mode, setMode] = useState<PaletteMode>(options?.mode ?? 'light');
+
+    const colorMode = useMemo(
+      () => ({
+        toggleColorMode: () => {
+          setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        },
+      }),
+      []
+    );
+
+    const theme = useMemo(
+      () =>
+        createTheme({
+          palette: {
+            mode,
+          },
+        }),
+      [mode]
+    );
+
+    return (
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <SWRConfig value={{ dedupingInterval: 0 }}>{children}</SWRConfig>
+          </LocalizationProvider>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    );
+  };
+
+  return render(ui, { wrapper: AllTheProviders, ...options });
+};
 
 export * from '@testing-library/react';
 
-export { customRender as render, AllTheProviders as wrapper };
+export { customRender as render };
