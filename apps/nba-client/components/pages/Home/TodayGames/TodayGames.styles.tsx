aa5440b95f0d8a8a0de8e5364813312cc81f7973
matiushariman <matius.hariman@gmail.com>
2022-12-30 12:25:45 +0700

    [nba-client] omit styles file from unit test coverage

---
+++ apps/nba-client/components/pages/Home/TodayGames/TodayGames.styles.tsx
@@ -0,0 +1,45 @@
+import { CSSObject, styled, Theme } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+
+const drawerWidth = 445;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+  ...(theme.palette.mode === 'light' && {
+    backgroundColor: theme.palette.grey.A200,
+  }),
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: 195,
+  ...(theme.palette.mode === 'light' && {
+    backgroundColor: theme.palette.grey.A200,
+  }),
+});
+
+export const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
+  }),
+}));
--- apps/nba-client/components/pages/Home/TodayGames/TodayGames.tsx
+++ apps/nba-client/components/pages/Home/TodayGames/TodayGames.tsx
@@ -1,10 +1,8 @@
-import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import TextField from '@mui/material/TextField';
 import Toolbar from '@mui/material/Toolbar';
-import { styled, Theme, CSSObject } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
@@ -13,54 +11,12 @@ import { useGetScoresWithSWR } from '@nba-app/api-client';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { formatDate } from '@nba-app/date-utils';
 
+import { Drawer } from './TodayGames.styles';
 import { TodayGame } from './TodayGame';
 import { MiniTodayGame } from './MiniTodayGame';
 
 import type { Dayjs } from 'dayjs';
 import type { TodayGamesProps } from './TodayGames.types';
-
-const drawerWidth = 445;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-  ...(theme.palette.mode === 'light' && {
-    backgroundColor: theme.palette.grey.A200,
-  }),
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: 195,
-  ...(theme.palette.mode === 'light' && {
-    backgroundColor: theme.palette.grey.A200,
-  }),
-});
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: 'nowrap',
-  boxSizing: 'border-box',
-  ...(open && {
-    ...openedMixin(theme),
-    '& .MuiDrawer-paper': openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    '& .MuiDrawer-paper': closedMixin(theme),
-  }),
-}));
 
 export const TodayGames = ({ games }: TodayGamesProps) => {
   const [selectedDate, setSelectedDate] = useState<Dayjs | Date>(new Date());
