55290990c9d17e4b2554cb6329b6cb61fc71946e
matiushariman <matius.hariman@gmail.com>
2023-01-01 17:14:58 +0700

    [ui] remove unnecessary ui-test-utils

--- libs/ui/src/lib/TeamLogo/__tests__/TeamLogo.spec.tsx
+++ libs/ui/src/lib/TeamLogo/__tests__/TeamLogo.spec.tsx
@@ -1,29 +1,11 @@
-import { render, screen } from '@nba-app/ui-test-utils';
+import { render, screen } from '../../../utils/testUtils';
 
 import { TeamLogo } from '../TeamLogo';
 
-jest.mock('@mui/material/styles', () => ({
-  ...(jest.requireActual('@mui/material/styles') as any),
-  useTheme: jest
-    .fn()
-    .mockReturnValueOnce({
-      palette: {
-        mode: 'light',
-      },
-    })
-    .mockReturnValueOnce({
-      palette: {
-        mode: 'dark',
-      },
-    }),
-}));
-
 describe(TeamLogo, () => {
-  beforeEach(() => {
-    render(<TeamLogo id="1" name="Celtics" />);
-  });
-
   it('should render L logo when using light theme', () => {
+    render(<TeamLogo id="1" name="Celtics" />);
+
     expect(screen.getByRole('img')).toHaveAttribute(
       'src',
       'https://cdn.nba.com/logos/nba/1/global/L/logo.svg'
@@ -31,6 +13,8 @@ describe(TeamLogo, () => {
   });
 
   it('should render D logo when using dark theme', () => {
+    render(<TeamLogo id="1" name="Celtics" />, { mode: 'dark' });
+
     expect(screen.getByRole('img')).toHaveAttribute(
       'src',
       'https://cdn.nba.com/logos/nba/1/global/D/logo.svg'
