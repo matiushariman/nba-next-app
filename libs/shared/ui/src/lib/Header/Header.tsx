c8cb1a16719a8666f1f705b26b9f85d6b85ea853
matiushariman <matius.hariman@gmail.com>
2023-01-12 13:04:51 +0700

    update Header container

--- libs/shared/ui/src/lib/Header/Header.tsx
+++ libs/shared/ui/src/lib/Header/Header.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image';
 
 export function Header() {
   return (
-    <header className="bg-gradient-to-r from-primary to-primary-light min-h-[64px] px-4 flex items-center">
-      <Image
-        src="https://cdn.nba.com/logos/leagues/logo-nba.svg"
-        alt="NBA"
-        width={52}
-        height={30}
-      />
+    <header className="from-primary to-primary-light flex min-h-[64px] items-center bg-gradient-to-r">
+      <div className="container mx-auto px-4">
+        <Image
+          src="https://cdn.nba.com/logos/leagues/logo-nba.svg"
+          alt="NBA"
+          width={52}
+          height={30}
+        />
+      </div>
     </header>
   );
 }
