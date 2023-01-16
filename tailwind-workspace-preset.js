3d97404471cb33586d8bdf684a8bc9e0478b28c2
matiushariman <matius.hariman@gmail.com>
2023-01-16 16:15:01 +0700

    init tailwindcss

---
+++ tailwind-workspace-preset.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#051B2D',
+        'primary-light': '#1C4289',
+      },
+    },
+  },
+};
